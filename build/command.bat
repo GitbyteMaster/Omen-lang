79e44a9fb6ec8cb9bbd66b873d3eb5493456b430
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-10-31 15:28:29 -0400

    Create command.bat

---
+++ build/command.bat
@@ -0,0 +1,11 @@
+IF "%1" == "exec" (
+  IF "%3" == "" (
+    IF EXIST %2 (
+      ECHO %2/ > C:\Users\%USERNAME%\exec
+    ) ELSE (
+      ECHO Omen: '%2' Doesn't exist! Check for typos, maybe?
+    )
+  ) ELSE (
+    ECHO Omen: Too many parameters!
+  )
+)

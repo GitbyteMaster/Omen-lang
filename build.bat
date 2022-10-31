25854da66b5cb577f127ebdf6b17a693c026a1ee
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-10-31 15:39:07 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 MKDIR C:\Users\%USERNAME%\Omen-lang
 MKDIR C:\Users\%USERNAME%\Omen-lang\assets
+MKDIR C:\Users\%USERNAME%\Omen-lang\build
+ECHO > C:\Users\%USERNAME%\Omen-lang\build\exec
 
 IF NOT ERRORLEVEL 0 (
   ECHO Omen: Failed to install Omen due to error: %ERRORLEVEL%

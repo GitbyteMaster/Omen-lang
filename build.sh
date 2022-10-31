ffea3b420dcf60f3afe41acdb17820996172fa90
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-10-31 15:16:17 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
 mkdir /Users/$USER/Omen-lang
+mkdir /Users/$USER/Omen-lang/assets
+
+if ! [ $? -eq 0 ];then
+  echo Omen: Failed to install Omen due to error: $?
+  echo Omen: Add an issue here: https://github.com/GitbyteMaster/Omen-lang/issues
+fi

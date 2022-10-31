d8c74cae1363aff762435880d6ff5ba0def83678
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-10-31 15:32:57 -0400

    Create command.sh

---
+++ build/command.sh
@@ -0,0 +1,11 @@
+if [[ "$1" == "exec" ]];then
+  if [[ "%3" == "" ]];then
+    if [[ -f $2 ]];then
+      echo $2\ > /Users/$USER/exec
+    else
+      ECHO "Omen: '%2' Doesn't exist! Check for typos, maybe?"
+    fi
+  else
+    echo "Omen: Too many parameters!"
+  fi
+fi

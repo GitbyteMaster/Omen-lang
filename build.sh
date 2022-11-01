5471d5d2a7e79fe7439f3e3f23c3bb942e8b3c15
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-11-01 16:00:42 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ mkdir /Users/$USER/Omen-lang
 mkdir /Users/$USER/Omen-lang/assets
 mkdir /Users/$USER/Omen-lang/build
 echo > /Users/$USER/Omen-lang/build/exec
-curl -S https://raw.githubusercontent.com/GitbyteMaster/Omen-lang/sys/build/verifier.py > /Users/$USER/omen-lang/build/verifier.py
+curl -S https://raw.githubusercontent.com/GitbyteMaster/Omen-lang/sys/build/verifier.py > /Users/$USER/Omen-lang/build/verifier.py
 curl -S https://raw.githubusercontent.com/GitbyteMaster/Omen-lang/sys/exbuild/command.sh > /Users/$USER/omen.sh
 
 if ! [ $? -eq 0 ];then

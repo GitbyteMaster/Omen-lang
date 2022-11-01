0f7e5bf67590a6d8c0c5502d8fed34d0123d90da
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-11-01 15:59:26 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ mkdir /Users/$USER/Omen-lang
 mkdir /Users/$USER/Omen-lang/assets
 mkdir /Users/$USER/Omen-lang/build
 echo > /Users/$USER/Omen-lang/build/exec
+curl -S https://raw.githubusercontent.com/GitbyteMaster/Omen-lang/sys/build/verifier.py > /Users/$USER/omen-lang/build/verifier.py
+curl -S https://raw.githubusercontent.com/GitbyteMaster/Omen-lang/sys/exbuild/command.sh > /Users/$USER/omen.sh
 
 if ! [ $? -eq 0 ];then
   echo Omen: Failed to install Omen due to error: $?

0c231cdd4eca77a476fa6bcf6a972a46fc0bf26d
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-11-01 15:56:33 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,11 @@
+@ECHO OFF
+
 MKDIR C:\Users\%USERNAME%\Omen-lang
 MKDIR C:\Users\%USERNAME%\Omen-lang\assets
 MKDIR C:\Users\%USERNAME%\Omen-lang\build
 ECHO > C:\Users\%USERNAME%\Omen-lang\build\exec
+CURL -S https://raw.githubusercontent.com/GitbyteMaster/Omen-lang/sys/build/verifier.py > C:\Users\%USERNAME%\omen.bat
+CURL -S https://raw.githubusercontent.com/GitbyteMaster/Omen-lang/sys/exbuild/command.bat > C:\Users\%USERNAME%\omen.bat
 
 IF NOT ERRORLEVEL 0 (
   ECHO Omen: Failed to install Omen due to error: %ERRORLEVEL%

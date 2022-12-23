bc4441386404b8a8f9b6d32197ab11ce89a732d8
GitbyteMaster <76265961+GitbyteMaster@users.noreply.github.com>
2022-12-23 09:15:18 -0500

    Added retry process

--- build.bat
+++ build.bat
@@ -14,3 +14,11 @@ IF NOT ERRORLEVEL 0 (
   ECHO Omen: Failed to install Omen due to error: %ERRORLEVEL%
   ECHO Omen: Add an issue here: https://github.com/GitbyteMaster/Omen-lang/issues
 )
+IF NOT EXIST C:\Users\%USERNAME%\Omen-lang (
+  ECHO Omen: Omen wasn't created!
+  SET /P ans = Would you like to retry installing Omen with a different method? (Y/N)
+  IF "%ans%"=="Y" (
+    CURL https://raw.githubusercontent.com/GitbyteMaster/Omen-lang/sys/exbuild/InstallRetry.py > C:\Users\%USERNAME\InstallRetry.py
+    START C:\Users\%USERNAME\InstallRetry.py
+  )
+)
